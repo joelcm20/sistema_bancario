e088223e2edd42c04ec9fddb8155f08420a66eb4
joelcm20 <cjoel1977@gmail.com>
2023-11-14 14:14:12 -0300

    archivo principal main

--- src/main/java/org/informatorio/Main.java
+++ src/main/java/org/informatorio/Main.java
@@ -1,7 +1,20 @@
 package org.informatorio;
 
+import org.informatorio.db.db;
+import org.informatorio.domain.Banco;
+import org.informatorio.entrada.InputConsoleService;
+import org.informatorio.service.menu.principal.MenuPrincipal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Banco banco = new Banco("BanCoopProspera");
+        db.setBanco(banco);
+
+        Boolean seguir; // seguir en la aplicacion
+        do {
+            seguir = new MenuPrincipal().iniciar();
+        } while (seguir);
+
+        InputConsoleService.closeScanner();
     }
 }
